e4296f8d3dcb7a2d228550762fc10a4ca8fbe56e
Jesse <dynaptx@163.com>
2022-07-08 22:29:25 +0800

    完成第二章练习题和编程题

--- Unit2_program/p8.c
+++ Unit2_program/p8.c
@@ -2,7 +2,21 @@
 
 int main()
 {
-  int a, b, c;
-  
-  
+  float a, b, c;
+  float s1, s2, s3;
+
+  printf("Enter amount of loan: ");
+  scanf("%f", &a);
+  printf("Enter inter rate: ");
+  scanf("%f", &b);
+  printf("Enter monthly payment: ");
+  scanf("%f", &c);
+
+  s1 = (a - c) + a * 0.06 / 12;
+  s2 = (s1 - c) + s1 * 0.06 / 12;
+  s3 = (s2 - c) + s2 * 0.06 / 12;
+
+  printf("Balance remaining after first payment: %.2f\n", s1);
+  printf("Balance remaining after second payment: %.2f\n", s2);
+  printf("Balance remaining after third payment: %.2f\n", s3);
 }
